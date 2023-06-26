3e01b91ac8e60735de1372e60fc832770d57499f
teojunda <teojunda@gmail.com>
2023-06-26 21:55:58 +0800

    implemented reply functionality for each post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import {
   Avatar,
   Box,
+  Button,
   Typography,
   Card,
   CardContent,
   CardHeader,
+  CardActions,
+  TextField,
 } from "@mui/material";
+import axios from "../apis/axios";
 
 const formatDate = dateString => {
   const options = { day: "numeric", month: "long", year: "numeric" };
@@ -32,6 +37,32 @@ const headerStyle = {
   alignItems: "center",
 };
 export default function Posts({ post }) {
+  const [expanded, setExpanded] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmitReply = async (e) => {
+    const username = (await axios.get("/user")).data;
+    await axios
+      .post("/addreply", {
+        date: new Date(),
+        username: username,
+        postId: post._id,
+        body: inputValue,
+      })
+      .then((res) => {
+        setInputValue("");
+        console.log(res);
+      });
+  };
+
   return (
     <Box style={postStyle}>
       <Card key={post._id} variant="outlined">
@@ -50,7 +81,26 @@ export default function Posts({ post }) {
           <Typography variant="caption" color="textSecondary">
             Date: {formatDate(post.date)}
           </Typography>
+          <TextField
+            id="reply-input"
+            multiline
+            rows={3}
+            placeholder="Reply ..."
+            variant="filled"
+            value={inputValue}
+            onChange={handleInputChange}
+            fullWidth
+          />
         </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSubmitReply}
+          >
+            Reply
+          </Button>
+        </CardActions>
       </Card>
     </Box>
   );
